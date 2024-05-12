1957dbc88924f6f61fbea55e56a8c6cb98405f41
alex cusack <alex.cusack@coinbase.com>
2024-05-11 23:41:07 -0700

    remove /app

--- app/config.js
+++
@@ -1,9 +0,0 @@
-import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
-
-export const config = createConfig({
-  chains: [mainnet, sepolia],
-  transports: {
-    [sepolia.id]: http(),
-  },
-})
